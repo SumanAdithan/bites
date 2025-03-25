3deaad09c318488b7ffcd818eed7ade77e447942
SumanAdithan <sumanadithang34@gmail.com>
2025-03-25 17:32:15 +0530

    feat: add redis setup

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { validate } from 'middlewares/validate.ts';
 import { Restaurant, RestaurantSchema } from 'schemas/restaurant.ts';
+import { initializeRedisClient } from 'utils/client.ts';
 const router = express.Router();
 
 router.post('/', validate(RestaurantSchema), async (request, response, next) => {
     const data = request.body as Restaurant;
+    const client = await initializeRedisClient();
     response.send('hello world');
 });
 
