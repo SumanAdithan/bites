acce8892b3efd6dbbc07d138c4f80620711de9a0
SumanAdithan <sumanadithang34@gmail.com>
2025-03-25 16:53:47 +0530

    feat: add zod validation

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from 'express';
+import { ZodSchema } from 'zod';
+
+export const validate =
+    <T>(schema: ZodSchema<T>) =>
+    (request: Request, response: Response, next: NextFunction) => {
+        const result = schema.safeParse(request.body);
+        if (!result.success) {
+            response.status(400).json({ success: false, errors: result.error.errors });
+            return;
+        }
+        next();
+    };
--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { validate } from 'middlewares/validate.ts';
+import { Restaurant, RestaurantSchema } from 'schemas/restaurant.ts';
 const router = express.Router();
 
-router.get('/', async (request, response) => {
+router.post('/', validate(RestaurantSchema), async (request, response, next) => {
+    const data = request.body as Restaurant;
     response.send('hello world');
 });
 
